d48aed6d9917eff8eda3753f5be9fa1d5c102c54
drelagreen <44087347+drelagreen@users.noreply.github.com>
2018-11-03 12:24:40 +0300

    03.11 2

--- SNChatClient/src/bin/ChatFrame.java
+++ SNChatClient/src/bin/ChatFrame.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.peer.TrayIconPeer;
 
 
 public class ChatFrame extends bin.Abstractions.ChatFrame {
